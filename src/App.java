83ca1dba842753c916bc05b2e3490ed29d3c8125
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2025-08-15 21:37:13 -0500

    Initial commit of the App.java file with main application logic.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 
 import controller.TaskControllers;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
@@ -14,5 +15,21 @@ public class App {
         controlador.completarTarea(1);
 
         controlador.mostrarTareas();
+
+        int [] numeros = {1,2,3,4};
+        
+        System.out.println(numeros.length);
+
+        String texto = "Hola";
+        System.out.println(texto.length());
+
+        ArrayList<Boolean> lista = new  ArrayList<>();
+
+        lista.add(true);
+        lista.add(true);
+        lista.add(true);
+        lista.add(false);
+
+        System.out.println(lista.size());
     }
 }
